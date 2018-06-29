6445934ae78b9d5771c32feb1bd281515bca2781
TangliziGit <tanglizimail@126.com>
2018-06-29 17:35:28 +0800

    more rational layout, and get messages automatically.

--- chatroom.py
+++ chatroom.py
@@ -1,19 +1,23 @@
+# TODO:
+# 1. use session
+# 2. use uniqe user_id instead of userName
+# 3. deal with secure problems
+# 4. maybe more beautiful?
+
 import json
 
 from flask import *
 from pymongo import MongoClient
 
 app=Flask(__name__)
-mcol=MongoClient().CARTROOM.messages
+mcol=MongoClient().ChatRoom.messages
 
 @app.errorhandler(404)
 def page_not_find(error):
     return render_template('404.html'), 404
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method=='POST':
-        abort(404)
     return render_template('index.html')
 
 @app.route('/post', methods=['POST'])
@@ -24,7 +28,6 @@ def get_message():
         msg={"time":int(form['time']), "user":form['user'],
                 'content':form['content']}
         mcol.insert(msg)
-        # ????????
         return "Succeed."
     return "Failed."
 
--- static/chatroom.js
+++ static/chatroom.js
@@ -1,11 +1,16 @@
+GetMessagesInterval=1000
+
 lastDate=new Date();
 
 $(document).ready(function(){
+    $("#getMsgBtn").hide();
+    $("#getMsgBtn").click(function(){getMessages(false)});
+
     $("#submitBtn").click(function(){
         lastDate=new Date();
         userName=$("#userNameBox").val();
 
-        $("div").append("<p>"+userName+": "+$("#textBox").val()+"</p>");
+        $("#chatBox").append("<p>"+userName+": "+$("#textBox").val()+"</p>");
         $.post("/post", {
             "time":lastDate.getTime(),
             "user":userName,
@@ -14,21 +19,25 @@ $(document).ready(function(){
             // alert("Data: "+data+"\nStatus:"+status)
         });
     });
-
-    $("#getMsgBtn").click(function(){
-        userName=$("#userNameBox").val();
-
-        getUrl="/get?time="+encodeURIComponent(lastDate.getTime())+
-            "&user="+encodeURIComponent(userName);
-        
-        lastDate=new Date();
-        $.get(getUrl, function(data, status){
-            // alert(data+"\n"+status);
-            items=jQuery.parseJSON(data);
-            // alert(items);
-            for (i in items){
-                $("div").append("<p>"+items[i]["user"]+": "+items[i]["content"]+"</p>");
-            }
-        });
-    })
 });
+
+function getMessages(recur){
+    userName=$("#userNameBox").val();
+    getUrl="/get?time="+encodeURIComponent(lastDate.getTime())+
+           "&user="+encodeURIComponent(userName);
+    
+    lastDate=new Date();
+    $.get(getUrl, function(data, status){
+        items=jQuery.parseJSON(data);
+        for (i in items){
+            $("#chatBox").append("<p>"+items[i]["user"]+": "+items[i]["content"]+"</p>");
+        }
+    });
+
+    if (recur){
+        setTimeout(function(){getMessages(true)}, GetMessagesInterval);
+    }
+}
+
+
+setTimeout(function(){getMessages(true)}, GetMessagesInterval)
